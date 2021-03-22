52d5bd43bc5730a71cfbbcce54087825b71fdeaf
arun <arunberry47@gmail.com>
2021-03-22 11:57:31 +0530

    adding product module with product data

---
+++ server/src/app/products/products-data.ts
@@ -0,0 +1,317 @@
+export const ProductData = [
+    {
+        "_id": "5ff1c9198f57ae0adeaf0a2c",
+        "__v": 0,
+        "brand": {
+            "_id": "5fdca0db69269602d4422dde",
+            "name": "Apple",
+            "code": "apple"
+        },
+        "channels": [
+            "ONLINE"
+        ],
+        "code": "iphone12_red_150GB",
+        "createdby": "hari balaji.m k",
+        "createddate": "2021-01-03T13:39:37.205Z",
+        "default": null,
+        "description": "testRed 150 GB ",
+        "dimension": {
+            "length": 120,
+            "width": 10,
+            "height": 10,
+            "name": "cm",
+            "code": "CENTIMETER"
+        },
+        "gallery": [
+            {
+                "id": 1,
+                "type": "image",
+                "media": {
+                    "shape": "box",
+                    "isDynamic": true,
+                    "url": "https://storage.googleapis.com/poorvika/product/4dcdc06a6de17658d9c9b7138197ede9.png",
+                    "width": 1200,
+                    "height": 1200,
+                    "id": 3
+                },
+                "default": false
+            },
+            {
+                "id": 2,
+                "type": "video",
+                "media": {
+                    "playerType": "Dailymotion",
+                    "url": "",
+                    "videoID": "x32bw0x",
+                    "controls": false,
+                    "loop": false,
+                    "id": "2"
+                },
+                "default": false
+            },
+            {
+                "id": 3,
+                "type": "threed",
+                "media": {
+                    "url": "https://storage.googleapis.com/poorvika/threedModel/6244152b80e2474aa3aa1fd7af82f69a.glb",
+                    "type": "glb",
+                    "id": "1",
+                    "img": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAy0lEQVR4nO3QPQrCQABE4RwgRCWpssafIonFYuMtRCxMKR5BrMSriMldx9oTjLDvg+mHl2UAAAAAACA5h8NJKY8A7gPuEcB9wD0CuA+4RwD3AfcsAaYY9N4HjbHWGGt99kFTDOkE6Jb5z/om167J0wlwDDOdQ6HrZqHbdqFLKHRaztIJ8E+zBBjWc93bUs+u0quv9GhLDat5OgH+aQRwH3CPAO4D7hHAfcA9ArgPuEcA9wH3COA+4B4B3AfcI4D7gHsEAAAAAAAAyfkC5f6F4ec3lI4AAAAASUVORK5CYII="
+                },
+                "default": false
+            }
+        ],
+        "gtin": "3984ulsdfhs",
+        "isActive": true,
+        "isbn": "",
+        "manufacturer": {
+            "_id": "5fd7443c7eb1f40d1835de25",
+            "name": "Apple"
+        },
+        "media": {
+            "image_gallery": [
+                {
+                    "shape": "box",
+                    "isDynamic": true,
+                    "url": "https://storage.googleapis.com/poorvika/product/4dcdc06a6de17658d9c9b7138197ede9.png",
+                    "width": 1200,
+                    "height": 1200,
+                    "id": 3
+                },
+                {
+                    "shape": "box",
+                    "isDynamic": true,
+                    "url": "https://storage.googleapis.com/poorvika/product/a167dffd2aa145c784c6d7d59548074e.png",
+                    "width": 1200,
+                    "height": 1200,
+                    "id": "2"
+                },
+                {
+                    "shape": "box",
+                    "isDynamic": true,
+                    "url": "https://storage.googleapis.com/poorvika/product/b50907468d1299d3cefb1561cd6068ba.png",
+                    "width": 1200,
+                    "height": 1200,
+                    "id": "1"
+                }
+            ],
+            "video_gallery": [
+                {
+                    "playerType": "Youtube",
+                    "url": "",
+                    "videoID": "nCNqPgXDYhY",
+                    "controls": false,
+                    "loop": false,
+                    "id": "1"
+                },
+                {
+                    "playerType": "Dailymotion",
+                    "url": "",
+                    "videoID": "x32bw0x",
+                    "controls": false,
+                    "loop": false,
+                    "id": "2"
+                }
+            ],
+            "threed_gallery": [
+                {
+                    "url": "https://storage.googleapis.com/poorvika/threedModel/6244152b80e2474aa3aa1fd7af82f69a.glb",
+                    "type": "glb",
+                    "id": "1",
+                    "img": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAy0lEQVR4nO3QPQrCQABE4RwgRCWpssafIonFYuMtRCxMKR5BrMSriMldx9oTjLDvg+mHl2UAAAAAACA5h8NJKY8A7gPuEcB9wD0CuA+4RwD3AfcsAaYY9N4HjbHWGGt99kFTDOkE6Jb5z/om167J0wlwDDOdQ6HrZqHbdqFLKHRaztIJ8E+zBBjWc93bUs+u0quv9GhLDat5OgH+aQRwH3CPAO4D7hHAfcA9ArgPuEcA9wH3COA+4B4B3AfcI4D7gHsEAAAAAAAAyfkC5f6F4ec3lI4AAAAASUVORK5CYII="
+                }
+            ]
+        },
+        "model": {
+            "_id": "5fdef406af112702941b1069",
+            "variant_attr_scheme": [
+                {
+                    "code": "color",
+                    "name": "Color",
+                    "values": [
+                        {
+                            "code": "red",
+                            "name": "Wine Red"
+                        },
+                        {
+                            "code": "green",
+                            "name": "Green"
+                        },
+                        {
+                            "code": "orange",
+                            "name": "Orange"
+                        }
+                    ]
+                },
+                {
+                    "code": "storage",
+                    "name": "Storage",
+                    "values": [
+                        {
+                            "code": "150GB",
+                            "name": "4GB RAM 64GB Storage"
+                        }
+                    ]
+                }
+            ],
+            "name": "Apple iPhone XR",
+            "tags": {
+                "brand": {
+                    "category": {
+                        "code": "IPHONE",
+                        "name": "iPhone"
+                    }
+                }
+            }
+        },
+        "modifiedby": "hari balaji.m k",
+        "modifieddate": "2021-03-18T13:44:19.672Z",
+        "name": "iPhone XR Red 64GB",
+        "number_facets": [],
+        "org_code": null,
+        "pageElementId": "6020ed4356ea360f7eb1d1a2",
+        "pageElements": [
+            {
+                "name": "Row",
+                "props": {
+                    "overflowH": false,
+                    "align": "normal",
+                    "justify": "start",
+                    "nowrap": false
+                },
+                "columns": [
+                    {
+                        "name": "Box",
+                        "props": {
+                            "colspan": 4
+                        },
+                        "components": [
+                            {
+                                "name": "Page Link",
+                                "props": {
+                                    "image": {
+                                        "shape": "box",
+                                        "width": 200,
+                                        "height": 200,
+                                        "isDynamic": true
+                                    },
+                                    "page": {},
+                                    "comp": {}
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ],
+        "pub_datetime": "2021-03-18T13:44:24.840Z",
+        "pub_v": 1.9,
+        "sku": "LLKSJDLK",
+        "specifications": {
+            "attributeShapeID": "6029c454f15e75027431482d",
+            "attributeGroups": [
+                {
+                    "code": "storage",
+                    "name": "Storage",
+                    "attributes": [
+                        {
+                            "code": "SDSTORAGE",
+                            "type": "INPUT",
+                            "name": "SD Storage",
+                            "value": "128Gb External Storage"
+                        },
+                        {
+                            "code": "RAM",
+                            "type": "SELECT",
+                            "name": "RAM",
+                            "value": {
+                                "code": "4GB",
+                                "text": "4GB"
+                            }
+                        },
+                        {
+                            "code": "InputNumber-5284e5",
+                            "type": "INPUTNUMBER",
+                            "name": "Input Number",
+                            "value": 2
+                        },
+                        {
+                            "code": "Radio-d05031",
+                            "type": "RADIO",
+                            "name": "Choice",
+                            "value": {
+                                "code": "16GB",
+                                "text": "16GB"
+                            }
+                        },
+                        {
+                            "code": "Boolean-e78af1",
+                            "type": "BOOLEAN",
+                            "name": "BooleanTest",
+                            "value": true
+                        }
+                    ]
+                }
+            ],
+            "attributeHighlights": [
+                {
+                    "code": "RAM",
+                    "name": "4GB"
+                },
+                {
+                    "code": "SDSTORAGE",
+                    "name": "128GB"
+                }
+            ]
+        },
+        "string_facets": [
+            {
+                "key": "storage",
+                "value": "64GB"
+            },
+            {
+                "key": "color",
+                "value": "red"
+            }
+        ],
+        "upc": "",
+        "variant": {
+            "img": {
+                "color": {
+                    "shape": "profile",
+                    "isDynamic": true,
+                    "url": "https://storage.googleapis.com/poorvika/prodvar/c11c20865a37ac75d0cbf2216f484a8f.png",
+                    "width": 400,
+                    "height": 400
+                }
+            },
+            "attr": {
+                "color": "red",
+                "storage": "150GB"
+            }
+        },
+        "weight": {
+            "value": 150,
+            "name": "g",
+            "code": "GRAM"
+        },
+        "tags": [
+            "bestseller"
+        ],
+        "seo": {
+            "title": "iPhone XR 64GB Red",
+            "metaTags": [
+                {
+                    "name": "description",
+                    "content": "iPhone 64Gb XR Red"
+                },
+                {
+                    "name": "keywords",
+                    "content": "64GB Red"
+                }
+            ]
+        },
+        "isPhysical": true,
+        "starrating": 3
+    }
+]
